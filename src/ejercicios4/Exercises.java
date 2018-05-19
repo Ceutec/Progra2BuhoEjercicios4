1163afa536be342cca65fd5da72cc8f11f6e9caa
turupawn <ahmed.hn.43@gmail.com>
2018-05-19 08:21:40 -0600

    first commit

---
+++ src/ejercicios4/Exercises.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios4;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author turupawn
+ */
+public class Exercises
+{
+    static int getSum(ArrayList<Integer> list)
+    {
+        return -1;
+    }
+    
+    static boolean exists(ArrayList<String> list, String value)
+    {
+        return false;
+    }
+    
+    static ArrayList<Integer> sort(ArrayList<Integer> list)
+    {
+        ArrayList<Integer> answer = new ArrayList<>();
+        return answer;
+    }
+}
